c24308ae5119ffd0199b36c88c2fc90c8bc0edbe
smnshzh <smnshzh@gmail.com>
2024-03-30 12:45:42 +0330

    seeting adding

---
+++ base_data/models.py
@@ -0,0 +1,59 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.db import models
+
+class EmployeeDetails(models.Model):
+    marital_status = models.CharField(max_length=50, verbose_name="وضعیت تاهل")
+    cost_center = models.CharField(max_length=50, verbose_name="مرکز هزینه")
+    employee_id = models.IntegerField(verbose_name="شماره پرسنلی")
+    insurance_type = models.CharField(max_length=50, verbose_name="نوع بیمه")
+    insurance_number = models.CharField(max_length=50, verbose_name="شماره بیمه")
+    employment_date = models.DateField(verbose_name="تاریخ استخدام")
+    date_of_birth = models.DateField(verbose_name="تاریخ تولد")
+    place_of_birth = models.CharField(max_length=100, verbose_name="محل تولد")
+    first_name = models.CharField(max_length=100, verbose_name="نام")
+    last_name = models.CharField(max_length=100, verbose_name="نام خانوادگی")
+    national_id = models.CharField(max_length=20, verbose_name="شماره شناسنامه")
+    father_name = models.CharField(max_length=100, verbose_name="نام پدر")
+    national_code = models.CharField(max_length=20, verbose_name="کد ملی")
+    postal_code = models.CharField(max_length=20, verbose_name="کد پستی")
+    telephone = models.CharField(max_length=20, verbose_name="تلفن")
+    mobile = models.CharField(max_length=20, verbose_name="تلفن همراه")
+    education_degree = models.CharField(max_length=100, verbose_name="مدرک تحصیلی")
+    field_of_study = models.CharField(max_length=100, verbose_name="رشته تحصیلی")
+    address = models.CharField(max_length=255, verbose_name="آدرس")
+    gender = models.CharField(max_length=10, verbose_name="جنسیت")
+    job_description = models.CharField(max_length=255, verbose_name="شرح شغل")
+    job_code = models.CharField(max_length=50, verbose_name="کد شغل")
+    contract_duration = models.IntegerField(verbose_name="مدت قرارداد(ماه)")
+    monthly_base_salary = models.DecimalField(max_digits=10, decimal_places=2, verbose_name="حقوق پایه ماهانه")
+    daily_base_salary = models.DecimalField(max_digits=10, decimal_places=2, verbose_name="حقوق پایه روزانه")
+    hourly_base_salary = models.DecimalField(max_digits=10, decimal_places=2, verbose_name="حقوق پایه هر ساعت")
+    recruitment_bonus = models.DecimalField(max_digits=10, decimal_places=2, verbose_name="حق جذب")
+    housing_allowance = models.DecimalField(max_digits=10, decimal_places=2, verbose_name="حق مسکن")
+    child_allowance = models.DecimalField(max_digits=10, decimal_places=2, verbose_name="حق اولاد")
+    food_and_weather_allowance = models.DecimalField(max_digits=10, decimal_places=2, verbose_name="بن و خواروبار")
+    weather_disability = models.DecimalField(max_digits=10, decimal_places=2, verbose_name="بدی آب و هوا")
+    mission_allowance = models.DecimalField(max_digits=10, decimal_places=2, verbose_name="حق ماموریت")
+    bonus = models.DecimalField(max_digits=10, decimal_places=2, verbose_name="پاداش")
+    insured = models.CharField(max_length=10, verbose_name="مشمول بیمه")
+    worker_share = models.DecimalField(max_digits=10, decimal_places=2, verbose_name="سهم کارگر")
+    employer_share = models.DecimalField(max_digits=10, decimal_places=2, verbose_name="سهم کارفرما")
+    two_thirds_exemption = models.DecimalField(max_digits=10, decimal_places=2, verbose_name="معافیت دو هفتم")
+    seniority = models.IntegerField(verbose_name="سنوات")
+    supervisor_allowance = models.DecimalField(max_digits=10, decimal_places=2, verbose_name="حق سرپرستی")
+    supplementary_insurance = models.DecimalField(max_digits=10, decimal_places=2, verbose_name="بیمه تکمیلی")
+    unemployment_insurance = models.DecimalField(max_digits=10, decimal_places=2, verbose_name="بیمه بیکاری")
+    termination_date = models.DateField(verbose_name="تاریخ ترک کار")
+    child_number = models.IntegerField(verbose_name="تعداد فرزند")
+    cstart = models.DateField(verbose_name="تاریخ شروع قرارداد")
+    cend = models.DateField(verbose_name="تاریخ پایان قرارداد")
+    user_creator = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name="Creator")
+
+    class Meta:
+        # Add a unique constraint for insurance_number and employee_id
+        unique_together = ['insurance_number', 'employee_id']
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name} ({self.employee_id})"
+
---
+++ base_data/views.py
@@ -0,0 +1,62 @@
+from django.shortcuts import render ,redirect
+import json
+import pandas as pd
+from django.http import JsonResponse
+def dashboard(request):
+    # Assuming you have some data to pass to the template
+    context = {
+        'welcome_message': 'Welcome to our website!',
+        'latest_news': ['News item 1', 'News item 2', 'News item 3'],
+        # Add more context data as needed
+    }
+    return render(request, 'home.html', context)
+
+def setting_view(request):
+
+
+    return render(request, 'setting.html')
+
+
+
+def insert_data(request):
+    if request.method == 'POST':
+        # Decode the JSON data from request body
+        user_name = request.user
+        json_data = json.loads(request.body)
+
+        # Check if JSON data is a list
+        if not isinstance(json_data, list):
+            return JsonResponse({'error': 'Input data must be a list'}, status=400)
+
+        # Check if JSON data contains at least two rows (header and data)
+        if len(json_data) < 2:
+            return JsonResponse({'error': 'Input data must contain header and data'}, status=400)
+
+        header = json_data[0]
+        data = json_data[1:]
+
+        # Check if the header contains all required columns
+        required_columns = ['مرکز هزینه', 'شماره پرسنلی', 'نوع بیمه', 'شماره بیمه', 'تاریخ استخدام', 'تاریخ تولد', 'محل تولد', 'نام', 'نام خانوادگی', 'شماره شناسنامه', 'نام پدر', 'کد ملی', 'کد پستی', 'تلفن', 'تلفن همراه', 'مدرک تحصیلی', 'رشته تحصیلی', 'آدرس', 'جنسیت', 'شرح شغل', 'کد شغل', 'مدت قرارداد(ماه)', 'حقوق پایه ماهانه', 'حقوق پایه روزانه', 'حقوق پایه هر ساعت', 'حق جذب', 'حق مسکن', 'حق اولاد', 'بن و خواروبار', 'بدی آب و هوا', 'حق ماموریت', 'پاداش', 'مشمول بیمه', 'سهم کارگر', 'سهم کارفرما', 'معافیت دو هفتم', 'سنوات', 'حق سرپرستی', 'بیمه تکمیلی', 'بیمه بیکاری', 'تاریخ ترک کار', 'وضعیت تاهل', 'تعداد فرزند', 'تاریخ شروع قرارداد', 'تاریخ پایان قرارداد']
+         # Add all required columns here
+        if not all(col in header for col in required_columns):
+            return JsonResponse({'error': 'Header is missing required columns'}, status=400)
+
+        # Check if data rows have the same number of columns as the header
+        if any(len(row) != len(header) for row in data):
+            return JsonResponse({'error': 'Data rows have inconsistent number of columns'}, status=400)
+
+        # Create DataFrame
+        df = pd.DataFrame(data, columns=header)
+
+        # Additional data validation checks if needed
+
+        # Assign user to DataFrame
+        df['user'] = user_name
+
+        # Print DataFrame
+        print(df)
+
+        return redirect('setting')
+
+    # Handle GET request or other cases
+    # You can render an upload template here
